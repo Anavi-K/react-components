a7e8ad5378099ce50a6e72f68f1bc77e084b0af2
Anavi-K <anavikothari@outlook.com>
2024-01-23 10:00:37 +0530

    updated

--- src/components/GalleryBody.jsx
+++ src/components/GalleryBody.jsx
@@ -4,15 +4,15 @@ export default function GalleryBody(props){
   const imageData = props.imageData;
   return(
     <div>
-{
-  imageData.map(image=>{
-    return(
-      <div key={image.id} className="column">
-        <img src={image.img} alt=""></img>
-      </div>
-    )
-  })
-}
-</div>
+      {
+        imageData.map(image=>{
+          return(
+            <div key={image.id} className="column">
+              <img src={image.img} alt=""></img>
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
